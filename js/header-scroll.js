d0e2067a548f9cebdd03298d4f65b55bf1da2044
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-10-18 05:31:08 +0200

    Add header search button

--- js/header-scroll.js
+++ js/header-scroll.js
@@ -1,5 +1,6 @@
 const header = document.querySelector(".header");
 const loginLink = document.querySelector(".header__login-link");
+const headerSearchButton = document.querySelector(".header__search-button");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 0) {
@@ -8,5 +9,11 @@ window.addEventListener("scroll", () => {
     } else {
         loginLink.classList.remove("header__login-link--scrolled");
         header.classList.remove("header--scrolled");
+    }
+
+    if (window.scrollY > 300) {
+        headerSearchButton.classList.add("header__search-button--scrolled");
+    } else {
+        headerSearchButton.classList.remove("header__search-button--scrolled");
     }
 });
